53dafd6b9614557902d68e93784e18ab74acd8ac
Dushyant Goswami <dushgoswami@gmail.com>
2019-12-15 10:54:20 +0530

    Creating first unit test for FinderFacadeService

--- src/app/core/finder-facade.service.spec.ts
+++ src/app/core/finder-facade.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FinderFacadeService } from './finder-facade.service';
+import { PlanetsService } from './planets.service';
+import { VehiclesService } from './vehicles.service';
+import { FalconFinderService } from './falcon-finder.service';
+import { Router } from '@angular/router';
 
-xdescribe('FinderFacadeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('FinderFacadeService', () => {
+
+  const planetServiceMock : PlanetsService = jasmine.createSpyObj(['getAllPlanets']);
+
+  const vehiclesServiceMock : VehiclesService = jasmine.createSpyObj(['getAllVehicles']);
+
+  const falconFinderServiceMock : FalconFinderService = jasmine.createSpyObj(['findFalcon']);
+
+  const routerServiceMock : Router = jasmine.createSpyObj(['navigate']);
+
+  beforeEach(() => TestBed.configureTestingModule({
+    providers : [
+      {provide : PlanetsService, useValue : planetServiceMock},
+      {provide : VehiclesService, useValue : vehiclesServiceMock} , 
+      {provide : FalconFinderService, useValue : falconFinderServiceMock}, 
+      {provide : Router, useValue : routerServiceMock}]
+  }));
 
   it('should be created', () => {
     const service: FinderFacadeService = TestBed.get(FinderFacadeService);
     expect(service).toBeTruthy();
   });
+  
 });
