74344cf37d713e276a58565398fd78533fa40978
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-04-15 17:37:04 +0530

    Categorizing into presenter and smart components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { FinderBoardComponent } from "./features/falcon-finder/containers/finder-board/finder-board.component";
-import { FalconeResultComponent } from "./features/falcon-finder/presenters/falcone-result/falcone-result.component";
-import { FalconeResetComponent } from "./features/falcon-finder/presenters/falcone-reset/falcone-reset.component";
-import { DestinationWidgetListComponent } from "./features/falcon-finder/presenters/destination-widget-list/destination-widget-list.component";
-import { DestinationWidgetComponent } from "./features/falcon-finder/presenters/destination-widget/destination-widget.component";
-import { VehicleListComponent } from "./features/falcon-finder/presenters/vehicle-list/vehicle-list.component";
-import { FalconHeaderComponent } from "./features/falcon-finder/presenters/falcon-header/falcon-header.component";
-import { FalconFooterComponent } from "./features/falcon-finder/presenters/falcon-footer/falcon-footer.component";
-import { Typeahead } from "./features/falcon-finder/presenters/typeahead/typeahead.component";
+import { FinderBoardComponent } from "./features/falcon-finder/smartComponents/finder-board/finder-board.component";
+import { FalconeResultComponent } from "./features/falcon-finder/smartComponents/falcone-result/falcone-result.component";
+import { FalconeResetComponent } from "./features/falcon-finder/presenterComponents/falcone-reset/falcone-reset.component";
+import { DestinationWidgetListComponent } from "./features/falcon-finder/smartComponents/destination-widget-list/destination-widget-list.component";
+import { DestinationWidgetComponent } from "./features/falcon-finder/smartComponents/destination-widget/destination-widget.component";
+import { VehicleListComponent } from "./features/falcon-finder/smartComponents/vehicle-list/vehicle-list.component";
+import { FalconHeaderComponent } from "./features/falcon-finder/smartComponents/falcon-header/falcon-header.component";
+import { FalconFooterComponent } from "./features/falcon-finder/presenterComponents/falcon-footer/falcon-footer.component";
+import { Typeahead } from "./features/falcon-finder/presenterComponents/typeahead/typeahead.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { AppComponent } from "./app.component";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CoreModule } from "./core/core.module";
-import { FinderBoardComponent } from "./features/falcon-finder/containers/finder-board/finder-board.component";
-import { DestinationWidgetListComponent } from "./features/falcon-finder/presenters/destination-widget-list/destination-widget-list.component";
-import { DestinationWidgetComponent } from "./features/falcon-finder/presenters/destination-widget/destination-widget.component";
-import { VehicleListComponent } from "./features/falcon-finder/presenters/vehicle-list/vehicle-list.component";
-import { FalconHeaderComponent } from "./features/falcon-finder/presenters/falcon-header/falcon-header.component";
-import { FalconFooterComponent } from "./features/falcon-finder/presenters/falcon-footer/falcon-footer.component";
-import { FalconeResultComponent } from "./features/falcon-finder/presenters/falcone-result/falcone-result.component";
-import { FalconeResetComponent } from "./features/falcon-finder/presenters/falcone-reset/falcone-reset.component";
-import { Typeahead } from "./features/falcon-finder/presenters/typeahead/typeahead.component";
+import { FinderBoardComponent } from "./features/falcon-finder/smartComponents/finder-board/finder-board.component";
+import { DestinationWidgetListComponent } from "./features/falcon-finder/smartComponents/destination-widget-list/destination-widget-list.component";
+import { DestinationWidgetComponent } from "./features/falcon-finder/smartComponents/destination-widget/destination-widget.component";
+import { VehicleListComponent } from "./features/falcon-finder/smartComponents/vehicle-list/vehicle-list.component";
+import { FalconHeaderComponent } from "./features/falcon-finder/smartComponents/falcon-header/falcon-header.component";
+import { FalconFooterComponent } from "./features/falcon-finder/presenterComponents/falcon-footer/falcon-footer.component";
+import { FalconeResultComponent } from "./features/falcon-finder/smartComponents/falcone-result/falcone-result.component";
+import { FalconeResetComponent } from "./features/falcon-finder/presenterComponents/falcone-reset/falcone-reset.component";
+import { Typeahead } from "./features/falcon-finder/presenterComponents/typeahead/typeahead.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
