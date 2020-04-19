91f6e7186ec9189919a6692bd94d19c7c4ef7237
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-04-19 12:48:27 +0530

    Showing Error Message to screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FinderFacadeService } from "./finder-board/services/finder-facade.service";
+import { takeWhile } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "findingFalcone";
+  constructor(public finderFacadeService: FinderFacadeService) {}
+  errorMessage = null;
+  isComponentActive = true;
+  ngOnInit() {
+    this.finderFacadeService.dashboardVm$
+      .pipe(takeWhile(() => this.isComponentActive))
+      .subscribe((vm) => (this.errorMessage = vm.error));
+  }
 }
