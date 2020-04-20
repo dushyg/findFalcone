69da153deada23ee5bdee8096c0e137742f8cd1b
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-04-20 14:41:33 +0530

    linting issue, replacing for loop with forEach

--- src/app/finder-board/utitlity.ts
+++ src/app/finder-board/utitlity.ts
@@ -1,9 +1,6 @@
-export const createSpyObj = (methodNames: []): { [key: string]: jest.Mock<any> } => {
+export const createSpyObj = (methodNames: string[]): { [key: string]: jest.Mock<any> } => {
     const obj: any = {};
 
-    // for (let i = 0; i < methodNames.length; i++) {
-    //     obj[methodNames[i]] = jest.fn();
-    // }
     methodNames.forEach((methodName) => {
         obj[methodName] = jest.fn();
     });
