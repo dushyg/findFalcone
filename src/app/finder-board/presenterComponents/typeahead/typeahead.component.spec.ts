c4e3bd50ed98c26f5bdbf4a1c765a4efdbe8c5d4
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-05-05 21:00:55 +0530

    More typeahead test cases

--- src/app/finder-board/presenterComponents/typeahead/typeahead.component.spec.ts
+++ src/app/finder-board/presenterComponents/typeahead/typeahead.component.spec.ts
@@ -3,6 +3,7 @@ import { TypeaheadComponent } from './typeahead.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
 import { IPlanet } from '../../models/planet';
+import { By } from '@angular/platform-browser';
 
 describe('TypeAhead Component', () => {
   let fixture: ComponentFixture<TypeaheadComponent>;
@@ -33,5 +34,77 @@ describe('TypeAhead Component', () => {
     fixture.detectChanges();
 
     expect(fixture.debugElement.componentInstance).toBeTruthy();
+  });
+
+  it('renders typeahaed input text box', () => {
+    component.sourceArray = [];
+    component.resetTypeAhead$ = of(null);
+
+    fixture.detectChanges();
+
+    expect(
+      fixture.debugElement.query(By.css('input.typeAhead[type=text]'))
+    ).toBeTruthy();
+  });
+
+  it('shows result list if input text box recieves focus', () => {
+    component.sourceArray = planets;
+    component.resetTypeAhead$ = of(null);
+
+    fixture.detectChanges();
+
+    const inputDebugElement = fixture.debugElement.query(
+      By.css('input.typeAhead[type=text]')
+    );
+    inputDebugElement.triggerEventHandler('focus', null);
+
+    fixture.detectChanges();
+
+    const resultListUlDebugElement = fixture.debugElement.query(
+      By.css('div.resultList ul')
+    );
+
+    expect(resultListUlDebugElement).toBeTruthy();
+  });
+
+  it('result list has expected values', () => {
+    component.sourceArray = planets;
+    component.resetTypeAhead$ = of(null);
+
+    fixture.detectChanges();
+
+    const inputDebugElement = fixture.debugElement.query(
+      By.css('input.typeAhead[type=text]')
+    );
+    inputDebugElement.triggerEventHandler('focus', null);
+
+    fixture.detectChanges();
+
+    // const resultListLiDebugElements = fixture.debugElement.queryAll(
+    //   By.css('div.resultList ul li')
+    // );
+
+    // expect(resultListLiDebugElements).toBeTruthy();
+    // expect(resultListLiDebugElements.length).toBe(planets.length + 1);
+    // expect(
+    //   (resultListLiDebugElements[0].nativeElement as HTMLElement).textContent
+    // ).toContain('None');
+    const resultListLiElements = (fixture.nativeElement as HTMLElement).querySelectorAll(
+      'div.resultList ul li'
+    );
+    expect(resultListLiElements).toBeTruthy();
+    expect(resultListLiElements.length).toBe(planets.length + 1);
+
+    resultListLiElements.forEach((li, index) => {
+      if (index > 0) {
+        expect((li as HTMLElement).textContent).toContain(
+          planets[index - 1].name
+        );
+      } else {
+        expect((resultListLiElements[0] as HTMLElement).textContent).toContain(
+          'None'
+        );
+      }
+    });
   });
 });
