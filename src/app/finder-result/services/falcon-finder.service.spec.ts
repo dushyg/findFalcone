89b197c6ffbccea4f0a3a15a9a6d291ee97b42dd
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-05-11 12:51:48 +0530

    Adding test ases for falcone finder service

--- src/app/finder-result/services/falcon-finder.service.spec.ts
+++ src/app/finder-result/services/falcon-finder.service.spec.ts
@@ -1,12 +1,183 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FalconFinderService } from './falcon-finder.service';
+import { TestScheduler } from 'rxjs/testing';
+import { HttpClient } from '@angular/common/http';
+import { IFindFalconeResponse } from '../models/findFalconeResponse';
+import { IFindFalconeRequest } from '../models/findFalconeRequest';
+import { of, throwError } from 'rxjs';
+import { RunHelpers } from 'rxjs/internal/testing/TestScheduler';
 
-xdescribe('FalconFinderService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('FalconFinderService', () => {
+  let testScheduler: TestScheduler;
 
-  it('should be created', () => {
-    const service: FalconFinderService = TestBed.get(FalconFinderService);
-    expect(service).toBeTruthy();
+  describe('FalconFinder success cases', () => {
+    const mockedHttpResponse = {
+      error: '',
+      planet_name: 'Donlon',
+      status: 'success',
+    };
+    const mockedFindFalconeResponse: IFindFalconeResponse = {
+      error: '',
+      planetName: 'Donlon',
+      status: 'success',
+    };
+
+    const httpClientMock = {
+      post: jest.fn(() => {
+        return of(mockedHttpResponse);
+      }),
+    };
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: HttpClient, useValue: httpClientMock },
+          FalconFinderService,
+        ],
+      });
+
+      testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    it('should be created', () => {
+      const service: FalconFinderService = TestBed.get(FalconFinderService);
+      expect(service).toBeTruthy();
+    });
+
+    it('should return mocked response when findFalcone is called', () => {
+      const service: FalconFinderService = TestBed.get(FalconFinderService);
+      const apiResponse = service.findFalcon({} as IFindFalconeRequest);
+      testScheduler.run(({ expectObservable }) => {
+        expectObservable(apiResponse).toBe('(a|)', {
+          a: mockedFindFalconeResponse,
+        });
+      });
+    });
+  });
+
+  describe('Falcon Finder failure to find cases', () => {
+    const mockedHttpResponse = {
+      status: 'false',
+    };
+    const mockedFindFalconeResponse: IFindFalconeResponse = {
+      error: undefined,
+      planetName: undefined,
+      status: 'false',
+    };
+
+    const httpClientMock = {
+      post: jest.fn(() => {
+        return of(mockedHttpResponse);
+      }),
+    };
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: HttpClient, useValue: httpClientMock },
+          FalconFinderService,
+        ],
+      });
+
+      testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    it('should return response with status of false when falcone is not found', () => {
+      const service: FalconFinderService = TestBed.get(FalconFinderService);
+      const apiResponse = service.findFalcon({} as IFindFalconeRequest);
+
+      testScheduler.run(({ expectObservable }) => {
+        expectObservable(apiResponse).toBe('(a|)', {
+          a: mockedFindFalconeResponse,
+        });
+      });
+    });
+  });
+
+  describe('Falcon Finder api error cases', () => {
+    const mockedHttpResponse = {
+      error:
+        'Token not initialized. Please get a new token with the /token API',
+    };
+    const mockedFindFalconeResponse: IFindFalconeResponse = {
+      error:
+        'Token not initialized. Please get a new token with the /token API',
+      planetName: undefined,
+      status: undefined,
+    };
+
+    const httpClientMock = {
+      post: jest.fn(() => {
+        return of(mockedHttpResponse);
+      }),
+    };
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: HttpClient, useValue: httpClientMock },
+          FalconFinderService,
+        ],
+      });
+
+      testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    it('should return response with error message when findFalcone api fails', () => {
+      const service: FalconFinderService = TestBed.get(FalconFinderService);
+      const apiResponse = service.findFalcon({} as IFindFalconeRequest);
+
+      testScheduler.run(({ expectObservable }) => {
+        expectObservable(apiResponse).toBe('(a|)', {
+          a: mockedFindFalconeResponse,
+        });
+      });
+    });
+  });
+
+  describe('Falcon Finder api exception cases', () => {
+    // const mockedFindFalconeResponse: IFindFalconeResponse = {
+    //   error:
+    //     'Communication exception',
+    //   planetName: undefined,
+    //   status: undefined,
+    // };
+
+    const mockedFindFalconeResponse = 'Communication exception';
+
+    const httpClientMock = {
+      post: jest.fn(() => {
+        return throwError(mockedFindFalconeResponse);
+      }),
+    };
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          { provide: HttpClient, useValue: httpClientMock },
+          FalconFinderService,
+        ],
+      });
+
+      testScheduler = new TestScheduler((actual, expected) => {
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    it('should return observable with error message when findFalcone api fails', () => {
+      const service: FalconFinderService = TestBed.get(FalconFinderService);
+      const apiResponse = service.findFalcon({} as IFindFalconeRequest);
+
+      testScheduler.run(({ expectObservable }: RunHelpers) => {
+        expectObservable(apiResponse).toBe(
+          '#',
+          null,
+          mockedFindFalconeResponse
+        );
+      });
+    });
   });
 });
