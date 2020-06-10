c896ee0c9839569ff0c410aa8f4257a8af104471
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-06-10 18:14:16 +0530

    Adding test ases for welcome page component

---
+++ src/app/welcome-page/welcome-page.component.spec.ts
@@ -0,0 +1,50 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WelcomePageComponent } from './welcome-page.component';
+import { Router } from '@angular/router';
+import { createSpyObj } from '../testingUtility';
+import { FinderFacadeService } from '../finder-board/services';
+
+describe('Welcome Page Component', () => {
+  let fixture: ComponentFixture<WelcomePageComponent>;
+  let component: WelcomePageComponent;
+  let routerServiceMock: { [key: string]: jest.Mock<any, any> };
+  let finderFacadeServiceMock;
+
+  beforeEach(() => {
+    routerServiceMock = createSpyObj(['navigate']);
+    finderFacadeServiceMock = {
+      setLoadingFlag: (loading) => {},
+    };
+    TestBed.configureTestingModule({
+      declarations: [WelcomePageComponent],
+      providers: [
+        { provide: Router, useValue: routerServiceMock },
+        { provide: FinderFacadeService, useValue: finderFacadeServiceMock },
+      ],
+    });
+    fixture = TestBed.createComponent(WelcomePageComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('Is created', () => {
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+    (expect(fixture) as any).toMatchSnapshot();
+  });
+
+  it('navigates to finderboard route when start button is clicked', () => {
+    fixture.detectChanges();
+    const loadingSpy = jest.spyOn(finderFacadeServiceMock, 'setLoadingFlag');
+    const routerSpy = jest.spyOn(routerServiceMock, 'navigate');
+    const btnStart: HTMLButtonElement = fixture.nativeElement.querySelector(
+      '.btnStart'
+    );
+
+    expect(btnStart).toBeTruthy();
+
+    btnStart.click();
+
+    expect(loadingSpy).toHaveBeenCalledWith(true);
+    expect(routerSpy).toHaveBeenCalledWith(['/finderboard']);
+  });
+});
