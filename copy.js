bebacccacc9ff598d77acf7790a1d6806dc246be
Dushyant Goswami <dushyant.goswami@yahoo.co.in>
2020-06-24 18:57:08 +0530

    2nd attempt to list travis files

--- copy.js
+++ copy.js
@@ -2,10 +2,15 @@ var args = process.argv.slice(2);
 console.log('args');
 console.log(args);
 
-require('fs').copyFile(args[0], args[1], function (err) {
+var path = require('path');
+var cwd = process.cwd();
+var source = path.join(cwd, args[0]);
+var destination = path.join(cwd, args[1]);
+console.log('source:' + source + ', dest:' + destination);
+require('fs').copyFile(source, destination, function (err) {
   if (err) {
     console.log(err);
     throw err;
   }
-  console.log(args[0] + ' successfully copied!');
+  console.log(source + ' successfully copied!');
 });
